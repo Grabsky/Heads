f21e2d57abd35928c0a35b66835910916e75bd99
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2025-06-22 14:33:15 +0200

    build changes

--- src/main/java/cloud/grabsky/heads/Heads.java
+++ src/main/java/cloud/grabsky/heads/Heads.java
@@ -15,20 +15,36 @@
 package cloud.grabsky.heads;
 
 import cloud.grabsky.bedrock.BedrockPlugin;
+import com.google.gson.Gson;
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.slf4j.Logger;
 import revxrsal.commands.Lamp;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.bukkit.BukkitLamp;
 import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.jetbrains.annotations.NotNull;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 public final class Heads extends BedrockPlugin {
 
@@ -94,6 +110,55 @@ public final class Heads extends BedrockPlugin {
             return "<dark_gray>› <gray>You have received all head variants for <gold><lang:" + type.translationKey() + "><gray>.";
         }
 
+    }
+
+    /* PLUGIN LOADER; FOR USE WITH PLUGIN-YML FOR GRADLE */
+
+    @SuppressWarnings("UnstableApiUsage")
+    public static final class PluginLoader implements io.papermc.paper.plugin.loader.PluginLoader {
+
+        @Override
+        public void classloader(final @NotNull PluginClasspathBuilder classpathBuilder) throws IllegalStateException {
+            final MavenLibraryResolver resolver = new MavenLibraryResolver();
+            // Parsing the file.
+            try (final InputStream in = getClass().getResourceAsStream("/paper-libraries.json")) {
+                final PluginLibraries libraries = new Gson().fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), PluginLibraries.class);
+                // Adding repositories to the maven library resolver.
+                libraries.asRepositories(classpathBuilder.getContext().getLogger()).forEach(resolver::addRepository);
+                // Adding dependencies to the maven library resolver.
+                libraries.asDependencies().forEach(resolver::addDependency);
+                // Adding library resolver to the classpath builder.
+                classpathBuilder.addLibrary(resolver);
+            } catch (final IOException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+        @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+        private static class PluginLibraries {
+
+            private final Map<String, String> repositories;
+            private final List<String> dependencies;
+
+            public Stream<RemoteRepository> asRepositories(final @NotNull Logger logger) {
+                return repositories.entrySet().stream().map(entry -> {
+                    try {
+                        // Replacing Maven Central repository with a pre-configured mirror.
+                        // See: https://docs.papermc.io/paper/dev/getting-started/paper-plugins/#loaders
+                        if (entry.getValue().contains("maven.org") == true || entry.getValue().contains("maven.apache.org") == true) {
+                            return new RemoteRepository.Builder(entry.getKey(), "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build();
+                        }
+                        return new RemoteRepository.Builder(entry.getKey(), "default", entry.getValue()).build();
+                    } catch (final NoSuchFieldError e) {
+                        return new RemoteRepository.Builder(entry.getKey(), "default", "https://maven-central.storage-download.googleapis.com/maven2").build();
+                    }
+                });
+            }
+
+            public Stream<Dependency> asDependencies() {
+                return dependencies.stream().map(value -> new Dependency(new DefaultArtifact(value), null));
+            }
+        }
     }
 
 }
